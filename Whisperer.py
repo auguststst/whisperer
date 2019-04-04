dd0ce339fb9a862a39b8141b1c62bc07ecbef4c3
August <auguststst@yandex.com>
2019-04-04 18:31:19 -0400

    must work

--- Whisperer.py
+++ Whisperer.py
@@ -5,8 +5,7 @@ import telebot
 import time
 import mysql.connector
 import re
-import os
-from flask import Flask, request
+
 
 
 
@@ -21,7 +20,6 @@ mydb = mysql.connector.connect(
 
 TOKEN = '801288104:AAFF3SCfE-iwEn9PDq6kAMSWdJ7OkyLZp7M'
 bot = telebot.TeleBot(token=TOKEN)
-server = Flask(__name__)
 
 usernames=[]  #new stroke
 
@@ -182,17 +180,8 @@ def handle_photo(message):
             mydb.commit()
             bot.send_message(message.chat.id, "Вы роспростронили слухи")
 
-        @server.route('/' + TOKEN, methods=['POST','GET'])
-        def getMessage():
-            bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-            return "!", 200
-
-        @server.route("/")
-        def webhook():
-            bot.remove_webhook()
-            bot.set_webhook(url='https://frozen-harbor-74862.herokuapp.com' + TOKEN)
-            return "!", 200
-
-        if __name__ == "__main__":
-            server.run(host="0.0.0.0", port=int(os.environ.get('PORT',5000)))
-            app.run()
+        while True:
+    try:
+        bot.polling(none_stop=True)
+    except Exception:
+        time.sleep(15)

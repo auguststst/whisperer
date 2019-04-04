d9efd0293274400e42f00ffe8c79a3371ea630c0
August <auguststst@yandex.com>
2019-04-04 19:55:13 -0400

    must work

--- Whisperer.py
+++ Whisperer.py
@@ -59,7 +59,7 @@ def handle_start(message):
 #new code
 
 
-@bot.message_handler(content_types=["text")
+@bot.message_handler(content_types=["text"])
 def handle_text(message):
 
     if '@' in message.text:

6cc0121559f34bcb3b1be9d68121af30fdbd2c3c
August <auguststst@yandex.com>
2019-04-06 14:05:44 -0400

    must work

--- Whisperer.py
+++ Whisperer.py
@@ -18,7 +18,7 @@ BUCKET_NAME = 'auguststst'
 
 bucket_name = AWS_ACCESS_KEY_ID.lower() + '-dump'
 conn = boto.connect_s3(AWS_ACCESS_KEY_ID,
-        AWS_SECRET_ACCESS_KEY)
+        AWS_ACCESS_SECRET_KEY)
 bucket = conn.create_bucket(bucket_name,
     location=boto.s3.connection.Location.DEFAULT)
 

da09cb5c8b32e102642f08b63924da74754a7501
August <auguststst@yandex.com>
2019-04-07 10:10:10 -0400

    must work

--- Whisperer.py
+++ Whisperer.py
@@ -195,6 +195,7 @@ def handle_photo(message):
 @server.route('/' + TOKEN, methods=['POST'])
 def getMessage():
     bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
 @server.route("/")
 def webhook():
@@ -203,5 +204,5 @@ def webhook():
     return "!", 200
 
 
-if __name__== "__main__":
+if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT',5000)))

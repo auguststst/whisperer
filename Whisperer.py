3faed22375e0a2b98722974642828bbc85fb7484
August <auguststst@yandex.com>
2019-04-07 07:35:09 -0400

    must work

--- Whisperer.py
+++ Whisperer.py
@@ -194,11 +194,16 @@ def handle_photo(message):
             bot.send_message(message.chat.id, "Вы роспростронили слухи")
 
 
-PORT = int(os.environ.get('PORT', '8443'))
-updater = Updater(TOKEN)
-# add handlers
-updater.start_webhook(listen="0.0.0.0",
-                      port=PORT,
-                      url_path=TOKEN)
-updater.bot.set_webhook('https://frozen-harbor-74862.herokuapp.com/' + TOKEN)
-updater.idle()
+@server.route('/' + TOKEN, methods=['POST'])
+def getMessage():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+
+@server.route("/")
+def webhook():
+    #bot.remove_webhook()
+    bot.set_webhook(url='https://frozen-harbor-74862.herokuapp.com/' + TOKEN)
+    return "!", 200
+
+
+if __name__== "__main__":
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT',5000)))
